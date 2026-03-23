d6c5005f79572366bd4e9ca22473fed0416ab4e2
Augusto <augusto20060309@gmail.com>
2026-03-23 11:00:34 -0400

    add insertBoard function to handle board creation in the database

---
+++ src/repository/boardRepository.ts
@@ -0,0 +1,10 @@
+import type { CreateBoardInput } from "../../types.ts";
+import { prisma } from "../../index.ts";
+
+export async function insertBoard(data: CreateBoardInput, userId: string) {
+    return await prisma.board.create({ data: {
+            name: data.name,
+            description: data.description,
+            ownerId: userId,
+            }, select: {name: true, description: true, ownerId: true}});
+}
