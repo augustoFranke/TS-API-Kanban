05d6396d71e913ca578643b9e5463f534fc73712
Augusto <augusto20060309@gmail.com>
2026-03-23 11:00:57 -0400

    refactor createBoard function to accept userId as parameter and improve user validation

--- src/services/boardService.ts
+++ src/services/boardService.ts
@@ -1,10 +1,13 @@
 import type { CreateBoardInput } from "../../types";
 import { AppError } from "../errors/AppError";
+import { insertBoard } from "../repository/boardRepository";
 import { findUserById } from "../repository/userRepository";
 
-export async function createBoard(data: CreateBoardInput) {
-    const existingId = await findUserById(data.userId);
-    if (existingId == null) {
+export async function createBoard(data: CreateBoardInput, userId: string) {
+    const existingId = await findUserById(userId);
+    if (existingId === null) {
         throw new AppError("User not found", 404);
     }
+
+    return await insertBoard(data, userId);
 }
