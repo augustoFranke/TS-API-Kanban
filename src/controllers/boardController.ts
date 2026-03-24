9a3403c3a1856e93927befb395766b100f17a2e1
Augusto <augusto20060309@gmail.com>
2026-03-24 10:53:09 -0400

    refactor: cast request to AuthenticatedReq to access userId in handleBoardCreation

--- src/controllers/boardController.ts
+++ src/controllers/boardController.ts
@@ -1,11 +1,13 @@
 import type { Request, Response } from "express";
+import type { AuthenticatedReq } from "../../types";
 import { boardSchema } from "../validators/boardSchemas";
 import { handleError } from "../errors/handleError";
 import { createBoard } from "../services/boardService"
 
 export async function handleBoardCreation(req: Request, res: Response) {
     try {
-        const userId = req.userId;
+        const authReq = req as AuthenticatedReq;
+        const userId = authReq.userId;
         const data = boardSchema.parse(req.body);
         const board = await createBoard(data, userId);
 
